c1cadeea5561aa7d7ff5d307efe4238ea56dd0af
Catalin Belu <catalin.belu@kantar.com>
2020-11-13 17:56:32 +0200

    migs added

---
+++ DAMProject/Database/Migrations/20201113155542_newfieldsininterview.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Database.Migrations
+{
+    public partial class newfieldsininterview : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DurationHours",
+                table: "Interviews",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "DurationMinutes",
+                table: "Interviews",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DurationHours",
+                table: "Interviews");
+
+            migrationBuilder.DropColumn(
+                name: "DurationMinutes",
+                table: "Interviews");
+        }
+    }
+}
