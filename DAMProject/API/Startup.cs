2cd70b34ebf62565832c6ae508ad5a839e61293c
danapaduraru <danapaduraru941@gmail.com>
2021-01-09 13:41:40 +0200

    update context backend

--- DAMProject/API/Startup.cs
+++ DAMProject/API/Startup.cs
@@ -29,7 +29,8 @@ namespace API
             services.AddDbContext<Context>(options =>
             {
                 //options.UseSqlServer(@"Data Source=DESKTOP-1PE5EKU\SQLEXPRESS;Initial Catalog=DAMProject; Trusted_Connection=True;");
-                options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DAM; Trusted_Connection=True;");
+                //options.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DAM; Trusted_Connection=True;");
+                options.UseSqlServer(@"Data Source=DESKTOP-RG20533\SQLEXPRESS;Initial Catalog=dam; Trusted_Connection=True;");
             });
 
             services.AddSingleton(DAMInfrastructure.AutoMapper.Configure().CreateMapper());
--- DAMProject/Database/Context.cs
+++ DAMProject/Database/Context.cs
@@ -16,7 +16,9 @@ namespace Database
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DAM; Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-1PE5EKU\SQLEXPRESS;Initial Catalog=DAMProject; Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=DAM; Trusted_Connection=True;");
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-RG20533\SQLEXPRESS;Initial Catalog=dam; Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
