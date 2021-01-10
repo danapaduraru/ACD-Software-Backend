8693cdc99f66bebfb5372380b6f01cfd91c268c8
danapaduraru <danapaduraru941@gmail.com>
2021-01-10 18:17:30 +0200

    added interviews page

--- DAMProject-Frontend/src/Constants.js
+++ DAMProject-Frontend/src/Constants.js
@@ -3,7 +3,8 @@ export const endpoints = {
     JOB_POSITION_BY_ID: "https://localhost:44395/JobPositions/",
     APPLICATIONS_FOR_JOB_POSITION: "https://localhost:44395/Applications/jobid/",
     APPLICATIONS_FOR_APPLICANT: "https://localhost:44395/Applications/applicantid/",
-    APPLICANT_BY_ID: "https://localhost:44395/Persons/"
+    APPLICANT_BY_ID: "https://localhost:44395/Persons/",
+    INTERVIEWS_FOR_PERSON_ID: "https://localhost:44395/Interviews/GetByPersonId/"
 };
 
 export default endpoints;
